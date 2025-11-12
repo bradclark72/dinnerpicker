ba928d32b23133456c66a1557fb37975c36f40cc
bradley clark <dinnerpicker72@gmail.com>
2025-11-12 18:57:17 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
-// This file is intentionally left blank.
-// The firebase config will be injected during the build process.
 export const firebaseConfig = {
-  projectId: 'studio-2822881531-c4670',
-  appId: '1:768823222257:web:6ef90257cea48b1c94ae50',
-  apiKey: 'AIzaSyAuJUXmvmAUtLIAxGxXfHrAkuYFJ4NVxKE',
-  authDomain: 'studio-2822881531-c4670.firebaseapp.com',
-  measurementId: 'G-1K5Y9LFE8G',
-  messagingSenderId: '768823222257',
+  "projectId": "studio-2822881531-c4670",
+  "appId": "1:768823222257:web:6ef90257cea48b1c94ae50",
+  "apiKey": "AIzaSyAuJUXmvmAUtLIAxGxXfHrAkuYFJ4NVxKE",
+  "authDomain": "studio-2822881531-c4670.firebaseapp.com",
+  "measurementId": "G-1K5Y9LFE8G",
+  "messagingSenderId": "768823222257"
 };
