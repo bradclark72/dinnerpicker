b6189ea666bd41d5ab3cd007dd2a96f418a76933
bradley clark <dinnerpicker72@gmail.com>
2025-11-13 18:35:05 +0000

    the mothly button is stil not working, do you need the itme link for tha

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,8 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   env: {
-    NEXT_PUBLIC_STRIPE_MONTHLY_PRICE_ID: process.env.STRIPE_MONTHLY_PRODUCT_ID,
-    NEXT_PUBLIC_STRIPE_LIFETIME_PRICE_ID: process.env.STRIPE_LIFETIME_PRODUCT_ID,
+    NEXT_PUBLIC_STRIPE_MONTHLY_PRICE_ID: process.env.STRIPE_MONTHLY_PRICE_ID,
+    NEXT_PUBLIC_STRIPE_LIFETIME_PRICE_ID: process.env.STRIPE_LIFETIME_PRICE_ID,
   },
   images: {
     remotePatterns: [
