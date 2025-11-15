50aa6a3239c9dd14cbdf8d64c4dd4992e9389811
bradley clark <dinnerpicker72@gmail.com>
2025-11-15 10:51:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2822881531-c4670",
+  "appId": "1:768823222257:web:6ef90257cea48b1c94ae50",
+  "apiKey": "AIzaSyAuJUXmvmAUtLIAxGxXfHrAkuYFJ4NVxKE",
+  "authDomain": "studio-2822881531-c4670.firebaseapp.com",
+  "measurementId": "G-1K5Y9LFE8G",
+  "messagingSenderId": "768823222257"
+};
